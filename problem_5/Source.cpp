a523b86b3f0231378a3a5c1db5fbd8b2ade1a53b
AfrizaIlhamzakyAbadi <ilhamzakyabadi@gmail.com>
2024-11-25 14:32:00 +0700

    Add files via upload

---
+++ problem_5/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool polindrome(string x, int N)
+{
+	int L{ 0 }, R{ N - 1 };
+	while (L < R)
+	{
+		if (x[L] == x[R])
+		{
+			L++;
+			R--;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+int main()
+{
+	string a = "qwewq";
+	string b = "qwe";
+	cout << polindrome(a, 5) << polindrome (b, 3);
+	return 0;
+}
